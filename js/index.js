5e11b39054007116c719b72077526f66efd1a57e
BCollignonEcv <baptiste.collignon@mail-ecv.fr>
2020-12-12 03:20:02 +0100

    update 11/12/2020

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 $('header .c_nav-icon').click(function(){
-    $('.c_nav-aside').toggleClass('active');
+    if($('.c_nav-aside').hasClass('open')){
+        $('.c_nav-aside').removeClass('open');
+        $('.c_nav-aside').addClass('close');
+    }else{
+        $('.c_nav-aside').addClass('open');
+        $('.c_nav-aside').removeClass('close');
+    }
 });
